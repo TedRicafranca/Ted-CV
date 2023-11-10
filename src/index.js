e8555ca0b07ef47e6048997a7c8554e3c3c8dfbb
tedricafranca <ted314089@gmail.com>
2023-11-10 21:45:48 +0800

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./assets/js/bootstrap.js";
+//import "./assets/js/bootstrap.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
